fb96f9745b4728eb17cbd362aefdfc24c4b313c5
Jonathan Campbell <jonathan@castus.tv>
2014-06-19 04:24:08 -0700

    push to github time!

---
+++ git-push-to-github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git remote add github https://joncampbell123@github.com/joncampbell123/hax-utils-1.git # might fail
+git remote set-url github https://joncampbell123@github.com/joncampbell123/hax-utils-1.git # might fail
+git push github master
+
